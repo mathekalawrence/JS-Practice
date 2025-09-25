19988fa1e4f8fa52f336c5930e3ae74c55b74c74
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-25 09:28:53 +0300

    Add files via upload

---
+++ more.js
@@ -0,0 +1,17 @@
+
+
+//<button onclick="alert('The Button is clicked')">Press Here</button>
+
+<button id='myButton'>LAWRENCE at JAVI</button>
+//DOM property assignment
+const button=document.getElementById('theBtn');
+button.onclick=function(){
+    alert('Here we go...stay awake!');
+};
+
+//using an addEventListener
+const button = document.getElementById('myButton');
+button.addEventListener('click', () => {
+    alert('button clicked!!');
+
+});
